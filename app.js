7fede1c95301ee0c7c93eac80328375f0f56caca
Bill Wu <71807347+all01280@users.noreply.github.com>
2024-05-16 16:07:34 +0800

    Update scroll reveal

--- app.js
+++ app.js
@@ -7,3 +7,12 @@ window.addEventListener("scroll", () => {
     nav.style.boxShadow = "0 10px 10px -10px #777";
   }
 });
+
+// scroll reveal
+ScrollReveal({
+  //reset: true,
+  distance: "80px",
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".main-area, .resume", { origin: "top" });
